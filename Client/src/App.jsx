a2025f38b8a9499682d011c97f8769e8fa074fb3
AyanBanerjee <ayanb918@gmail.com>
2023-07-05 13:40:32 +0530

    Check
    Co-authored-by: itsKD007 <itsKD007@users.noreply.github.com>
    Co-authored-by: DebarshiChoudhury10 <choudhurydebarshi10@gmail.com>
    Co-authored-by: Indranil Patra <simplyrin2@users.noreply.github.com>

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className=" text-5xl font-bold underline text-red-400">Vite with Tailwind</h1>
+      <h1 className=" text-3xl font-bold underline text-red-400">Vite with Tailwind</h1>
     </>
   )
 }
