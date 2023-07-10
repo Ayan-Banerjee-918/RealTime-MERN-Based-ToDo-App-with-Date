388cbcdccc06cdc10be6b8495ec12abf7a7b4d9c
AyanBanerjee <ayanb918@gmail.com>
2023-07-10 20:08:35 +0530

    bugfix

--- Backend/connect.js
+++ Backend/connect.js
@@ -17,12 +17,7 @@ const io = new Server(server, {
 
 app.use(express.json());
 app.use(cors());
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.options('*', cors());
 
 const connectDatabase = (module.exports = () => {
     const params = {
