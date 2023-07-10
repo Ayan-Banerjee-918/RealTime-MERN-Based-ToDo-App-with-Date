4b3b730b1f282604c0edfef23491db475c245071
AyanBanerjee <ayanb918@gmail.com>
2023-07-10 20:25:04 +0530

    fixing CORS

--- Backend/connect.js
+++ Backend/connect.js
@@ -20,6 +20,12 @@ app.use(cors({
     origin: "*",
 }));
 
+app.options("*",function(req,res,next){
+    res.header("Access-Control-Allow-Origin", req.get("Origin")||"*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next()
+})
+
 const connectDatabase = (module.exports = () => {
     const params = {
         useNewUrlParser: true,
