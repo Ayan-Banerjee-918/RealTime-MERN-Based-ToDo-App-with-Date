3ebb82a243deed15074ee4570b4832fecf2437ba
AyanBanerjee <ayanb918@gmail.com>
2023-07-10 20:18:09 +0530

    bugfixes

--- Backend/connect.js
+++ Backend/connect.js
@@ -16,7 +16,9 @@ const io = new Server(server, {
 });
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "*",
+}));
 
 const connectDatabase = (module.exports = () => {
     const params = {
