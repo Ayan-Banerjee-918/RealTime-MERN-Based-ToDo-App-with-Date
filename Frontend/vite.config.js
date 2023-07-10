742f84517dc76d8a424977e7bcd348af0b05e240
AyanBanerjee <ayanb918@gmail.com>
2023-07-11 00:48:54 +0530

    vite config changes

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': process.env
+  },
+  build: {
+    target: 'esnext'
   }
 })
